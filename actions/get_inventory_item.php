efbfe98a61f92c98fcebc1a96f7193f91f5c328b
Vincent Crame <vincentcrame7@gmail.com>
2026-05-19 08:27:06 +0800

    add: left join for supply supplier

--- actions/get_inventory_item.php
+++ actions/get_inventory_item.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['inventory_id'])) {
     
     $sql = "SELECT i.*, s.supplier_name 
             FROM inventory i 
-            LEFT JOIN supplier s ON i.supplier_id = s.supplier_id 
+            LEFT JOIN supply_supplier s ON i.supplier_id = s.supplier_id 
             WHERE i.inventory_id = ?";
     
     $stmt = $conn->prepare($sql);
