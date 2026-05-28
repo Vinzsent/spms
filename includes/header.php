5438b3fbf76540b678d4c7ea188d6442cf6e0732
Vincent Crame <vincentcrame7@gmail.com>
2026-05-28 15:12:01 +0800

    add: header

--- includes/header.php
+++ includes/header.php
@@ -19,6 +19,10 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.bootstrap5.min.css">
 
+  <!-- Select2 CSS & Bootstrap 5 Theme -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css">
+
   <!-- Custom styles -->
   <style>
     :root {
